73b7fe07a94e5f2fb04b355cc55e58a0ad3ad886
Dylan Richard Muir <dylan.muir@synsense.ai>
2023-10-12 17:07:09 +0200

    * Added plot for RZCC llustration * Updated random phase plot * Modified STHT plots for paper figure

---
+++ paper_plots/rzcc_plots.py
@@ -0,0 +1,138 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# This module produces plots for evaluating the Hilbert transform for harmonic signals
+#
+#
+# (C) Saeid Haghighatshoar
+# email: saeid.haghighatshoar@synsense.ai
+#
+#
+# last update: 08.00.2023
+# ----------------------------------------------------------------------------------------------------------------------
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.signal import hilbert, lfilter, freqz
+from micloc.spike_encoder import ZeroCrossingSpikeEncoder
+
+from pathlib import Path
+import os
+from tqdm import tqdm
+
+
+def use_latex():
+    matplotlib.use("pdf")
+    matplotlib.rcParams.update(
+        {
+            "axes.spines.right": False,
+            "axes.spines.top": False,
+            "axes.linewidth": 0.5,
+            "lines.linewidth": 1.0,
+            "grid.linewidth": 0.5,
+            #     "pgf.texsystem": "xelatex",
+            #     "font.family": "Helvetica",
+            #     "text.usetex": True,
+            #     "pgf.rcfonts": False,
+        }
+    )
+
+    SMALL_SIZE = 6
+    MEDIUM_SIZE = 8
+    BIGGER_SIZE = 10
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=SMALL_SIZE)  # fontsize of the figure title
+
+
+
+SAVE_PLOTS = True
+
+if SAVE_PLOTS:
+    use_latex()
+
+
+def plot_RZCC():
+    # find the directory for this file
+    root = os.path.join(Path(__file__).resolve().parent, "RZCC_plots")
+
+    if not os.path.exists(root):
+        os.mkdir(root)
+
+    # parameters of the system
+    fs = 48_000
+    freq_list = np.asarray([500])
+
+    # signal duration
+    sig_duration = 1 / np.min(freq_list)
+    sig_len = int(fs * sig_duration)
+    time_vec = np.linspace(0, sig_duration, sig_len)
+
+    mm = 1/25.4
+
+
+    plt.show()
+
+    for freq in tqdm(freq_list):
+        filename = os.path.join(root, f"rzcc_freq={freq}Hz.pdf")
+        
+        mm = 1/25.4
+        
+        sig_in = np.cos(2 * np.pi * freq * time_vec)
+        # Make a spike encoder
+        freq_range = [freq / 2, 2 * freq]
+        zc_dist = int(fs / freq_range[-1])
+        robust_width = zc_dist // 2
+
+        print(robust_width)
+        rzcc = ZeroCrossingSpikeEncoder(fs, robust_width)
+
+        plt.figure(figsize=[60 * mm, 40 * mm])
+        sig_cat = np.stack([sig_in, sig_in])
+        spikes = rzcc.evolve(sig_cat.T)
+        # spikes[spikes < 0] = 0
+
+        plt.plot(time_vec * 1e3, sig_in, label="input signal")
+
+        plt.plot(time_vec * 1e3, np.cumsum(sig_in) / 10 - 3.5, 'C0--')
+
+        plt.plot(0.5 + robust_width * np.array([-1, 1]) / fs * 1e3, [-1.5, -1.5], 'k-')
+
+        # plt.plot(spikes)
+        plt.stem(time_vec * 1e3, spikes[:, 0]/2, 'C3-', markerfmt='', basefmt='none')
+
+        plt.plot(time_vec[[0, -1]] * 1e3, [0, 0], 'k:')
+        # plt.legend()
+        plt.grid(False)
+        plt.axis('off')
+        # plt.xlabel("Time (ms)")
+        # plt.ylabel("Amplitude")
+        # plt.title(
+        #     f"STHT: f: {freq} Hz, {r'$f_s$'}: {int(fs/1000)} KHz, kernel-dur: {1000*win_duration:0.1f} ms"
+        # )
+
+        if SAVE_PLOTS:
+            plt.savefig(filename, bbox_inches="tight", transparent=True)
+        else:
+            plt.draw()
+
+
+        print(
+            f"simulation was done for freq: {freq} and plot was saved in file:{filename}"
+        )
+
+    if SAVE_PLOTS:
+        pass
+    else:
+        plt.show()
+
+
+def main():
+    plot_RZCC()
+
+
+if __name__ == "__main__":
+    main()
--- paper_plots/short_hilbert_transform.py
+++ paper_plots/short_hilbert_transform.py
@@ -12,24 +12,48 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert, lfilter, freqz
+from micloc.spike_encoder import ZeroCrossingSpikeEncoder
 
 from pathlib import Path
 import os
 from tqdm import tqdm
 
+
 def use_latex():
-    matplotlib.use("pgf")
-    matplotlib.rcParams.update({
-        "pgf.texsystem": "xelatex",
-        'font.family': 'serif',
-        'text.usetex': True,
-        'pgf.rcfonts': False,
-    })
+    matplotlib.use("pdf")
+    matplotlib.rcParams.update(
+        {
+            "axes.spines.right": False,
+            "axes.spines.top": False,
+            "axes.linewidth": 0.5,
+            "lines.linewidth": 1.0,
+            "grid.linewidth": 0.5,
+            #     "pgf.texsystem": "xelatex",
+            #     "font.family": "Helvetica",
+            #     "text.usetex": True,
+            #     "pgf.rcfonts": False,
+        }
+    )
+
+    SMALL_SIZE = 6
+    MEDIUM_SIZE = 8
+    BIGGER_SIZE = 10
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=SMALL_SIZE)  # fontsize of the figure title
+
+
 
 SAVE_PLOTS = True
 
 if SAVE_PLOTS:
     use_latex()
+
 
 def plot_STHT():
     # find the directory for this file
@@ -51,42 +75,53 @@ def plot_STHT():
     ker_h = np.fft.fftshift(np.imag(hilbert(impulse)))
 
     # signal duration
-    sig_duartion = 4 / np.min(freq_list)
-    sig_len = int(fs * sig_duartion)
-    time_vec = np.linspace(0, sig_duartion, sig_len)
+    sig_duration = 4 / np.min(freq_list)
+    sig_len = int(fs * sig_duration)
+    time_vec = np.linspace(0, sig_duration, sig_len)
 
     # compute the frequency response
     num_freq_samples = 10_000
-    w, h = freqz(ker_h, a=1, worN=num_freq_samples, whole=True, plot=None, fs=fs, include_nyquist=True)
+    w, h = freqz(
+        ker_h,
+        a=1,
+        worN=num_freq_samples,
+        whole=True,
+        plot=None,
+        fs=fs,
+        include_nyquist=True,
+    )
     w[w >= fs / 2] = w[w >= fs / 2] - fs
 
     # print the kernel
-    plt.figure(figsize=(6,6.5))
-    filename = os.path.join(root, "kernel.pgf")
+    mm = 1/25.4
+
+    plt.figure(figsize=[70*mm, 90*mm])
+    filename = os.path.join(root, "kernel.pdf")
 
     plt.subplot(211)
     plt.plot(np.arange(0, len(ker_h)) / fs * 1000, ker_h)
-    plt.xlabel("time (ms)")
+    plt.plot([0, len(ker_h) / fs * 1000], [0, 0], 'k:')
+    plt.xlabel("Time (ms)")
     plt.ylabel("STHT Kernel")
-    plt.title(fr"STHT kernel: duration: {int(win_duration * 1000):0.1f} ms, $f_s$: {int(fs / 1000)} KHz")
-    plt.grid(True)
+    plt.title(
+        rf"STHT kernel: Duration {int(win_duration * 1000):0.1f} ms, $F_s$: {int(fs / 1000)} KHz"
+    )
+    plt.grid(False)
 
     plt.subplot(212)
-    plt.plot(w/1000, 20 * np.log10(np.abs(h)))
+    plt.plot(w / 1000, 20 * np.log10(np.abs(h)))
     plt.ylim([-5, 5])
-    plt.ylabel("STHT freq response [dB]")
-    plt.xlabel("freq (KHz)")
-    plt.grid(True)
+    plt.ylabel("STHT response (dB)")
+    plt.xlabel("F (KHz)")
+    plt.grid(False)
 
     if SAVE_PLOTS:
-        plt.savefig(filename)
+        plt.savefig(filename, bbox_inches="tight", transparent=True)
     else:
         plt.draw()
 
-
-
     # plt.figure()
-    # filename = os.path.join(root, "kernel_freq.pgf")
+    # filename = os.path.join(root, "kernel_freq.pdf")
     #
     # plt.subplot(211)
     # plt.plot(w, 20*np.log10(np.abs(h)))
@@ -107,40 +142,75 @@ def plot_STHT():
 
     plt.show()
 
-
     for freq in tqdm(freq_list):
-        filename = os.path.join(root, f"freq={freq}Hz.pgf")
-        plt.figure()
+        filename = os.path.join(root, f"freq={freq}Hz.pdf")
+        
+        mm = 1/25.4
+        
+        plt.figure(figsize=[70 * mm, 45 * mm])
 
-        sig_in = np.sin(2 * np.pi * freq * time_vec)
+        sig_in = np.sin(2 * np.pi * freq * time_vec) + np.random.normal(scale = 0.025, size = len(time_vec))
         sig_h = sig_in + 1j * lfilter(ker_h, [1], sig_in)
 
         sig_hilbert = hilbert(sig_in)
 
-        plt.plot(time_vec, np.real(sig_h), label="STHT-I <= input signal")
-        plt.plot(time_vec, np.imag(sig_h), label="STHT-Q")
-        plt.plot(time_vec, np.imag(sig_hilbert), label="Hilbert-Q")
-        plt.legend()
-        plt.grid(True)
-        plt.xlabel("time (sec)")
-        plt.ylabel("signals")
-        plt.title(f"STHT: f: {freq} Hz, {r'$f_s$'}: {int(fs/1000)} KHz, kernel-dur: {1000*win_duration:0.1f} ms")
+        plt.plot(time_vec * 1e3, np.real(sig_h), label="STHT-I <= input signal")
+        plt.plot(time_vec * 1e3, np.imag(sig_h), color = 'C1', label="STHT-Q")
+        plt.plot(time_vec * 1e3, np.imag(sig_hilbert), '--', color = 'black', label="Hilbert-Q")
+        plt.plot(time_vec[[0, -1]] * 1e3, [0, 0], 'k:')
+        # plt.legend()
+        plt.grid(False)
+        plt.xlabel("Time (ms)")
+        plt.ylabel("Amplitude")
+        # plt.title(
+        #     f"STHT: f: {freq} Hz, {r'$f_s$'}: {int(fs/1000)} KHz, kernel-dur: {1000*win_duration:0.1f} ms"
+        # )
 
         if SAVE_PLOTS:
-            plt.savefig(filename)
+            plt.savefig(filename, bbox_inches="tight", transparent=True)
         else:
             plt.draw()
 
-        print(f"simulation was done for freq: {freq} and plot was saved in file:{filename}")
+
+        # Make a spike encodder
+        freq_range = [freq / 2, 2 * freq]
+        zc_dist = int(fs / freq_range[-1])
+        robust_width = zc_dist // 2
+
+        print(robust_width)
+        rzcc = ZeroCrossingSpikeEncoder(fs, robust_width)
+
+        plt.figure(figsize=[70 * mm, 5 * mm])
+        sig_cat = np.stack([np.real(sig_h), np.imag(sig_h)])
+        spikes = rzcc.evolve(sig_cat.T)
+        # spikes[spikes < 0] = 0
+        
+        # plt.plot(spikes)
+        plt.stem(time_vec * 1e3, -spikes[:, 0], 'C0-', markerfmt='', basefmt='none')
+        plt.stem(time_vec * 1e3, -spikes[:, 1], 'C1-', markerfmt='', basefmt='none')
+        plt.grid(False)
+        plt.xlabel("Time (ms)")
+        plt.yticks([])
+        plt.axis('off')
+        # plt.ylabel("Amplitude")
+        
+        filename = os.path.join(root, f"spikes_freq={freq}Hz.pdf")
+        plt.savefig(f"{filename}", bbox_inches = "tight", transparent=True)
+
+
+        print(
+            f"simulation was done for freq: {freq} and plot was saved in file:{filename}"
+        )
 
     if SAVE_PLOTS:
         pass
     else:
         plt.show()
 
+
 def main():
     plot_STHT()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
