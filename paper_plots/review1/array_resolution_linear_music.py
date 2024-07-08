f160bcfa4bc3e7ca3e9ec5834330d66b5c0ac6e0
haghighatshoar <haghighatshoar@gmail.com>
2024-07-08 15:02:21 +0200

    simulation files for linear array geometry were added. this is to be completed soon ...

---
+++ paper_plots/review1/array_resolution_linear_music.py
@@ -0,0 +1,207 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# This module plots the array resolution for the MUSIC algorithm.
+#
+# NOTE: This simulation is specifically for review1 and addresses the linear array geometry rather than the circular one used
+# in the first submission.
+#
+#
+# (C) Saeid Haghighatshoar
+# email: saeid.haghighatshoar@synsense.ai
+#
+#
+# last update: 08.07.2024
+# ----------------------------------------------------------------------------------------------------------------------
+from operator import ge
+import numpy as np
+
+from micloc.array_geometry import LinearArray
+from micloc.music_beamformer import MUSIC
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import matplotlib
+import os
+from pathlib import Path
+from tqdm import tqdm
+
+
+def use_latex():
+    matplotlib.use("pdf")
+    matplotlib.rcParams.update(
+        {
+            "axes.spines.right": False,
+            "axes.spines.top": False,
+            "axes.linewidth": 0.5,
+            "lines.linewidth": 1.0,
+            "grid.linewidth": 0.5,
+            "xtick.major.width": 0.5,
+            "ytick.major.width": 0.5,
+            "xtick.major.size": 2,
+            "ytick.major.size": 2,
+            #     "pgf.texsystem": "xelatex",
+            #     "font.family": "Helvetica",
+            #     "text.usetex": True,
+            #     "pgf.rcfonts": False,
+        }
+    )
+
+    SMALL_SIZE = 6
+    MEDIUM_SIZE = 8
+    BIGGER_SIZE = 10
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=SMALL_SIZE)  # fontsize of the figure title
+
+
+SAVE_PLOTS = True
+
+if SAVE_PLOTS:
+    use_latex()
+
+
+def plot_beampattern(doa_list, beam1, beam2, title, filename):
+    mm = 1 / 25.4
+
+    plt.figure(figsize=[35 * mm, 35 * mm])
+    gs = gridspec.GridSpec(1, 1)
+    ax1 = plt.subplot(gs[0], polar=True)
+    # ax2 = plt.subplot(gs[1], polar=False)
+
+    ax1.plot(doa_list, np.abs(beam1), label="beam pattern")
+    ax1.plot(doa_list, np.abs(beam2), label="beam pattern")
+    ax1.set_title(title)
+    ax1.grid(True)
+    ax1.set_xticks(np.arange(0 / 180 * np.pi, 360 / 180 * np.pi, 60 / 180 * np.pi))
+    ax1.set_yticks([0.25, 0.5, 0.75, 1.0])
+    ax1.set_yticklabels([])
+
+    if SAVE_PLOTS:
+        plt.savefig(filename, bbox_inches="tight", transparent=True)
+    else:
+        plt.draw()
+
+
+def array_resolution_sin():
+    """
+    this function computes the array resolution curve at various angle via direct numerical simulation.
+    the template signals used for localization are sinusoids.
+    """
+    # find the directory for this file
+    root = os.path.join(Path(__file__).resolve().parent, "array_resolution_music_sin")
+
+    if not os.path.exists(root):
+        os.mkdir(root)
+
+    # build a geometry
+    radius = 4.5e-2
+    whole_span = 2*radius
+    num_mic = 7
+    spacing = whole_span/num_mic
+    fs = 48_000
+
+    #geometry = CenterCircularArray(radius=radius, num_mic=num_mic)
+    geometry = LinearArray(
+        spacing=spacing,
+        num_mic=num_mic,
+    )
+
+    # build beamformer matrix for various DoAs
+    # 1. build a template signal
+    duration = 0.4
+    num_fft_bin = 2048
+    freq_design_vec = [1000, 2000, 3600, 4000, 8000]
+
+    # use an angular grid
+    num_grid = 64 * num_mic + 1
+    doa_list = np.linspace(-np.pi, np.pi, num_grid) + np.pi
+
+    for freq_design in tqdm(freq_design_vec):
+        print(f"\n\nplotting beam pattern for freq: {freq_design}...")
+
+        filename = os.path.join(root, f"array_resolution_sin_freq={freq_design}.pdf")
+
+        # frequency range and numebr of active frequencies
+        freq_range = [0.8 * freq_design, 1.2 * freq_design]
+        num_active_freq = 1
+
+        beamf = MUSIC(
+            geometry=geometry,
+            freq_range=freq_range,
+            doa_list=doa_list,
+            frame_duration=duration,
+            fs=fs,
+        )
+
+        time_temp = np.arange(0, duration, step=1 / fs)
+        sig_temp = np.sin(2 * np.pi * freq_design * time_temp)
+
+        ## compute two beampatterns
+        ang_pow_spec1 = beamf.apply_to_template(
+            template=[time_temp, sig_temp, 0],
+            num_active_freq=num_active_freq,
+            duration_overlap=0.0,
+            num_fft_bin=num_fft_bin,
+            snr_db=1000,
+        )
+
+        # accumulate all angular power spectrum in tim
+        beam_pattern1 = ang_pow_spec1.mean(0)
+        beam_pattern1 = beam_pattern1 / beam_pattern1.max()
+
+        ang_pow_spec2 = beamf.apply_to_template(
+            template=[time_temp, sig_temp, np.pi / 4],
+            num_active_freq=num_active_freq,
+            duration_overlap=0.0,
+            num_fft_bin=num_fft_bin,
+            snr_db=1000,
+        )
+
+        # accumulate all angular power spectrum in tim
+        beam_pattern2 = ang_pow_spec2.mean(0)
+        beam_pattern2 = beam_pattern2 / beam_pattern2.max()
+
+        plot_beampattern(
+            doa_list,
+            beam_pattern1,
+            beam_pattern2,
+            f"$F= {freq_design / 1000:0.0f}$ kHz",
+            filename,
+        )
+
+        # plt.figure()
+        # gs = gridspec.GridSpec(2, 1, height_ratios=[1.8, 1])
+        # ax1 = plt.subplot(gs[0], polar=True)
+        # ax2 = plt.subplot(gs[1], polar=False)
+
+        # ax1.plot(doa_list, beam_pattern1, label="beam pattern")
+        # ax1.plot(doa_list, beam_pattern2, label="beam pattern")
+        # ax1.set_title(
+        #     f"array resolution: freq= {freq_design / 1000:0.1f} KHz")
+        # ax1.grid(True)
+
+        # ax2.plot(doa_list / np.pi * 180, beam_pattern1)
+        # ax2.plot(doa_list / np.pi * 180, beam_pattern2)
+        # ax2.legend(["DoA: 0", "DoA: 90"])
+        # ax2.set_xlabel("DoA")
+        # ax2.set_ylabel("array resolution")
+        # ax2.grid(True)
+
+        # if SAVE_PLOTS:
+        #     plt.savefig(filename)
+        # else:
+        #     plt.draw()
+
+    if not SAVE_PLOTS:
+        plt.show()
+
+
+def main():
+    array_resolution_sin()
+
+
+if __name__ == "__main__":
+    main()
---
+++ paper_plots/review1/array_resolution_linear_snn.py
@@ -0,0 +1,290 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# This module plots the array resolution for the Hilbert-transform based localization algorithm.
+# This module obtains array beam pattern for the SNN version.
+#
+# NOTE: this file was added for review1 to address checking another geometry for multi-mic array.
+# NOTE: in this file, we address a linear array geometry rather than the circular one used for the previous submission.
+#
+# (C) Saeid Haghighatshoar
+# email: saeid.haghighatshoar@synsense.ai
+#
+#
+# last update: 08.07.2024
+# ----------------------------------------------------------------------------------------------------------------------
+import numpy as np
+
+from micloc.array_geometry import LinearArray
+from micloc.snn_beamformer import SNNBeamformer
+from micloc.utils import Envelope
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import matplotlib
+from scipy.signal import lfilter, butter
+import os
+from pathlib import Path
+from tqdm import tqdm
+
+
+def use_latex():
+    matplotlib.use("pdf")
+    matplotlib.rcParams.update(
+        {
+            "axes.spines.right": False,
+            "axes.spines.top": False,
+            "axes.linewidth": 0.5,
+            "lines.linewidth": 1.0,
+            "grid.linewidth": 0.5,
+            "xtick.major.width": 0.5,
+            "ytick.major.width": 0.5,
+            "xtick.major.size": 2,
+            "ytick.major.size": 2,
+            #     "pgf.texsystem": "xelatex",
+            #     "font.family": "Helvetica",
+            #     "text.usetex": True,
+            #     "pgf.rcfonts": False,
+        }
+    )
+
+    SMALL_SIZE = 6
+    MEDIUM_SIZE = 8
+    BIGGER_SIZE = 10
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=SMALL_SIZE)  # fontsize of the figure title
+
+
+SAVE_PLOTS = True
+
+if SAVE_PLOTS:
+    use_latex()
+
+
+def plot_beampattern(doa_list, corr, title, filename):
+    mm = 1 / 25.4
+
+    plt.figure(figsize=[35 * mm, 35 * mm])
+    gs = gridspec.GridSpec(1, 1)
+    ax1 = plt.subplot(gs[0], polar=True)
+    # ax2 = plt.subplot(gs[1], polar=False)
+
+    ax1.plot(doa_list, np.abs(corr[0]), label="beam pattern")
+    ax1.plot(doa_list, np.abs(corr[len(corr)//8]), label="beam pattern")
+    #ax1.plot(doa_list, np.abs(corr[2*len(corr)//16]), label="beam pattern")
+    ax1.set_title(title)
+    ax1.grid(True)
+    ax1.set_xticks(np.arange(0/180*np.pi, 45 / 180 * np.pi, 360 / 180 * np.pi))
+    ax1.set_yticks([0.25, 0.5, 0.75, 1.0])
+    ax1.set_yticklabels([])
+
+    # selected_indices = np.arange(0, len(corr), len(corr) // 4)
+    # selected_doa = doa_list[selected_indices]
+    # ax2.plot(doa_list / np.pi * 180, corr[selected_indices, :].T)
+    # ax2.legend([f"DoA: {int(rad / np.pi * 180)}" for rad in selected_doa])
+    # ax2.set_xlabel("DoA")
+    # ax2.set_ylabel("array resolution")
+    # ax2.grid(True)
+
+    if SAVE_PLOTS:
+        plt.savefig(filename, bbox_inches="tight", transparent=True)
+    else:
+        plt.draw()
+
+
+def array_resolution_sin():
+    """
+    this function computes the array resolution curve at various angle via direct numerical simulation.
+    """
+    # find the directory for this file
+    root = os.path.join(Path(__file__).resolve().parent, "array_resolution_snn_sin")
+
+    if not os.path.exists(root):
+        os.mkdir(root)
+
+    # build a geometry
+    radius = 4.5e-2
+    whole_span = 2*radius
+    num_mic = 7
+    spacing = whole_span/num_mic
+    fs = 48_000
+
+    #geometry = CenterCircularArray(radius=radius, num_mic=num_mic)
+    geometry = LinearArray(
+        spacing=spacing,
+        num_mic=num_mic,
+    )
+
+    # build the corresponding beamformer
+    kernel_duration = 10e-3
+    freq_range = [500, 8500]
+
+    # build beamformer matrix for various DoAs
+    # 1. build a template signal
+    duration = 0.6
+    freq_design_vec = [1000, 2000, 3600, 4000, 8000]
+
+    for freq_design in tqdm(freq_design_vec):
+        # design a specific beamformer
+        freq_range = [freq_design / 2, 2 * freq_design]
+        tau_mem = 1 / (2 * np.pi * freq_design)
+        tau_syn = 1 / (2 * np.pi * freq_design)
+        tau_vec = [tau_mem, tau_syn]
+
+        # bipolar spikes
+        bipolar_spikes = False
+        beamf = SNNBeamformer(
+            geometry=geometry,
+            kernel_duration=kernel_duration,
+            freq_range=freq_range,
+            tau_vec=tau_vec,
+            bipolar_spikes=bipolar_spikes,
+            fs=fs,
+        )
+
+        print(f"\n\nplotting beam pattern for freq: {freq_design}...")
+
+        filename = os.path.join(
+            root, f"array_resolution_snn_sin_freq={freq_design}.pdf"
+        )
+
+        time_temp = np.arange(0, duration, step=1 / fs)
+        sig_temp = np.sin(2 * np.pi * freq_design * time_temp)
+
+        # 2. use an angular grid
+        num_grid = 64 * num_mic + 1
+        doa_list = np.linspace(-np.pi, np.pi, num_grid) + np.pi
+
+        bf_mat = beamf.design_from_template(
+            template=(time_temp, sig_temp), doa_list=doa_list
+        )
+
+        # obtain the complex version of the beamforming
+        bf_mat_comp = bf_mat[:num_mic, :] + 1j * bf_mat[num_mic:, :]
+
+        # plot the array resolution
+        corr = np.abs(bf_mat_comp.conj().T @ bf_mat_comp)
+
+        plot_beampattern(
+            doa_list, corr, f"$F= {freq_design / 1000:0.0f}$ kHz", filename
+        )
+
+    if not SAVE_PLOTS:
+        plt.show()
+
+
+def array_resolution_wideband():
+    """
+    this function computes the array resolution curve at various angle via direct numerical simulation.
+    """
+    # find the directory for this file
+    root = os.path.join(
+        Path(__file__).resolve().parent, "array_resolution_snn_wideband"
+    )
+
+    if not os.path.exists(root):
+        os.mkdir(root)
+
+    # build a geometry
+    radius = 4.5e-2
+    whole_span = 2*radius
+    num_mic = 7
+    spacing = whole_span/num_mic
+    fs = 48_000
+
+    #geometry = CenterCircularArray(radius=radius, num_mic=num_mic)
+    geometry = LinearArray(
+        spacing=spacing,
+        num_mic=num_mic,
+    )
+
+    # build the corresponding beamformer
+    kernel_duration = 10e-3
+
+    # minimum frequency for which STHT can works
+    f_min = 10 / kernel_duration
+
+    # build beamformer matrix for various DoAs
+    # 1. build a template signal
+    duration = 0.6
+    bandwidth = 1000
+    center_freq_vec = [1000, 2000, 3600, 4000, 8000]
+
+    for center_freq in tqdm(center_freq_vec):
+        print(
+            f"\n\nplotting beam pattern for wideband signal of bandwidth: {center_freq}..."
+        )
+
+        # design a specific beamformer
+        freq_range = [center_freq / 2, 2 * center_freq]
+        tau_mem = 1 / (2 * np.pi * center_freq)
+        tau_syn = 1 / (2 * np.pi * center_freq)
+        tau_vec = [tau_mem, tau_syn]
+
+        # frequency range of the array
+        # freq_range = [f_min, f_min + bandwidth]
+        freq_range = [center_freq - bandwidth / 2, center_freq + bandwidth / 2]
+
+        # build the beamformer for the specific scenario
+        bipolar_spikes = False
+
+        beamf = SNNBeamformer(
+            geometry=geometry,
+            kernel_duration=kernel_duration,
+            freq_range=freq_range,
+            tau_vec=tau_vec,
+            bipolar_spikes=bipolar_spikes,
+            fs=fs,
+        )
+
+        # build a filter for the array
+        # butterworth filter parameters
+        N = 2
+        b, a = butter(N, freq_range, btype="pass", analog=False, output="ba", fs=fs)
+
+        filename = os.path.join(
+            root, f"array_resolution_snn_wideband_fc={center_freq}.pdf"
+        )
+
+        time_temp = np.arange(0, duration, step=1 / fs)
+        noise = np.random.randn(len(time_temp))
+
+        sig_temp = lfilter(b, a, noise)
+
+        # 2. use an angular grid
+        num_grid = 32 * num_mic + 1
+        doa_list = np.linspace(-np.pi, np.pi, num_grid) + np.pi
+
+        bf_mat = beamf.design_from_template(
+            template=(time_temp, sig_temp), doa_list=doa_list
+        )
+
+        # obtain the complex version of the beamforming matrix
+        bf_mat_comp = bf_mat[:num_mic, :] + 1j * bf_mat[num_mic:, :]
+
+        # plot the array resolution
+        # corr = np.abs(bf_mat.conj().T @ bf_mat)
+        corr = np.abs(bf_mat_comp.conj().T @ bf_mat_comp)
+
+        plot_beampattern(
+            doa_list,
+            corr,
+            f"$F_c= {center_freq / 1000:0.0f}$ kHz, $B={bandwidth / 1000:0.0f}$ kHz",
+            filename,
+        )
+
+    if not SAVE_PLOTS:
+        plt.show()
+
+
+def main():
+    array_resolution_sin()
+    # plot_beampattern()
+    # array_resolution_wideband()
+
+
+if __name__ == "__main__":
+    main()
